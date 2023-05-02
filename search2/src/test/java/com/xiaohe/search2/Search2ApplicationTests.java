dfe2b7dd68fd69f2b5f3ec8c17437d49a824069a
2382546457 <2382546457@qq.com>
2023-05-02 17:34:18 +0800

    更改查找结果，从单个String改为list<String>

---
+++ search2/src/test/java/com/xiaohe/search2/Search2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xiaohe.search2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Search2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
